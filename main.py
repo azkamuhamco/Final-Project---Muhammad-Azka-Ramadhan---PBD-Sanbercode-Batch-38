f48956b1bb4999647e20841a90719f64c7fe7395
unknown <reprokbas@gmail.com>
2022-10-10 11:14:48 +0700

    update main.py

--- main.py
+++ main.py
@@ -11,18 +11,30 @@ def header(tabel):
     cursor_db.close()
     return [i[0] for i in result]
 
-@app.get('/products')
-async def products():
-    query = "SELECT * FROM products"
+def data(tabel):
+    query = "SELECT * FROM " + str(tabel)
     cursor_db = db.cursor()
     cursor_db.execute(query)
     result = cursor_db.fetchall()
     cursor_db.close()
 
-    heder   = header("products")
+    heder   = header(tabel)
     allData = []
     for i in range(len(result)):
         itemData    = [[heder[j]] + [result[i][j]] for j in range(len(heder))]
         dicItemData = dict((x, y) for x, y in itemData)
         allData.append(dicItemData)
     return allData
+
+def searchProductById(id):
+    for product in data("products"):
+        print(product.get('product_id'))
+        if product.get('product_id') == (int(id)):
+            return product
+    return {"result" :"Tidak Ditemukan"}
+
+@app.get('/products')
+async def products():
+    return data("products")
+
+
