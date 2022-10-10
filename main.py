340931cec4d0840db343c92c8bf012f912907f99
unknown <reprokbas@gmail.com>
2022-10-10 11:53:29 +0700

    selesai tidak selesai

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from constant import db
+from re import search
 
 app = FastAPI()
 
@@ -28,13 +29,35 @@ def data(tabel):
 
 def searchProductById(id):
     for product in data("products"):
-        print(product.get('product_id'))
         if product.get('product_id') == (int(id)):
             return product
     return {"result" :"Tidak Ditemukan"}
 
 @app.get('/products')
-async def products():
+async def show_all_products():
     return data("products")
 
+@app.get("/product/{id}")
+async def get_product_by_id(id):
+    return searchProductById(id)
 
+@app.get("/product_search")
+async def search_country_by_name(name):
+    for product in data("products"):
+        if search(str(name), product.get('product_name')):
+            return product
+    return {"result" :"Tidak Ditemukan"}
+
+def searchUserById(id):
+    for user in data("users"):
+        if user.get('customer_id') == (int(id)):
+            return user
+    return {"result" :"Tidak Ditemukan"}
+
+@app.get('/users')
+async def show_all_users():
+    return data("users")
+
+@app.get("/user/{id}")
+async def get_user_by_id(id):
+    return searchUserById(id)
